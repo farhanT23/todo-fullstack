05fcab271bf04485cca78d5f8c62e6f899bf0ba3
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-07-23 16:42:36 +0600

    Initial Commit

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React default dev port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Todo API is running!"}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-8xl">hi</p>
+    </>
+  )
+}
+
+export default App
