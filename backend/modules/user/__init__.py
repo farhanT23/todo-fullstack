9a1de4eccc5e179216d6b3ef54d2e4991f23d649
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-07-24 11:32:51 +0600

    bugfix: token removed creating user

--- backend/modules/user/__init__.py
+++ backend/modules/user/__init__.py
@@ -7,8 +7,9 @@ from utils.error_response import ErrorResponse
 from middlewares.auth import get_current_user, optional_get_current_user
 
 from utils.database import get_db
-from modules.user.schema import UserCreate, UserLoginResponseSchema, Token, UserLogin
+from modules.user.schema import UserCreate, UserLoginResponseSchema, Token, UserLogin, UserOut
 from modules.user.service import UserService
+from fastapi.responses import JSONResponse
 
 
 router = APIRouter(
@@ -29,20 +30,16 @@ async def health():
     return {"message": "Up"}
 
 
-@router.post('/',response_model=UserLoginResponseSchema,status_code=status.HTTP_201_CREATED)
+@router.post('/',response_model=UserOut,status_code=status.HTTP_201_CREATED)
 async def create_user(request:Request,user:UserCreate,db:AsyncSession=Depends(get_db)):
 
     service = UserService(db)
     user = await service.create_user(user)
-    response = UserLoginResponseSchema(
-            user=user,
-            token=Token(
-                access_token=await service.genarate_token(user),
-                refresh_token=await service.refresh_token(user)
-            )
-        )
-
-    return response
+    
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"message": "User Created Successfully"}
+    )
 
 @router.post('/login', response_model=UserLoginResponseSchema, status_code=status.HTTP_200_OK)
 async def login(credentials: UserLogin, db:AsyncSession=Depends(get_db)):
